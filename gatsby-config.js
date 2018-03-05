6ce0b8b1b28e71efede17fefb0c321df83c108eb
Dan Kass <romeboards@gmail.com>
2018-03-05 12:51:17 -0500

    anayltics and offline plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,6 +33,15 @@ module.exports = {
           })
         ]
       }
-    }
-  ],
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-67069242-4",
+        // Puts tracking script in the head instead of the body
+        head: false
+      }
+    },
+    `gatsby-plugin-offline`
+  ]
 }
