0d611ca08a9197489e88d05d074054b2fcdbb84f
Dan Kass <romeboards@gmail.com>
2018-03-26 14:46:49 -0400

    tweaking babel config

--- gatsby-node.js
+++ gatsby-node.js
@@ -29,10 +29,22 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
 exports.modifyWebpackConfig = ({ config, stage }) => {
   const program = {
     directory: __dirname,
-    browserslist: ["> 1%", "last 2 versions", "IE >= 10"],
+    browserslist: ["> 1%", "last 2 versions", "IE > 9"],
   };
 
-  return generateBabelConfig(program, stage);
+  return generateBabelConfig(program, stage).then(babelConfig => {
+    config.removeLoader("js").loader("js", {
+      test: /\.jsx?$/,
+      exclude: modulePath => {
+        return (
+          /node_modules/.test(modulePath) &&
+          !/node_modules\/(swiper|dom7)/.test(modulePath)
+        );
+      },
+      loader: "babel",
+      query: babelConfig,
+    });
+  });
 };
 
 // exports.modifyWebpackConfig = ({ config, stage }) => {
