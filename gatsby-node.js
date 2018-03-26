1d7d8d7c512b107fed42d9e46ce7b0ec3a58c71a
Dan Kass <romeboards@gmail.com>
2018-03-26 15:00:25 -0400

    trying babelrc

--- gatsby-node.js
+++ gatsby-node.js
@@ -26,26 +26,26 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
 //   plugins: babelrc.plugins.concat(['transform-regenerator']),
 // });
 
-exports.modifyWebpackConfig = ({ config, stage }) => {
-  const program = {
-    directory: __dirname,
-    browserslist: ["> 1%", "last 2 versions", "IE > 9"],
-  };
-
-  return generateBabelConfig(program, stage).then(babelConfig => {
-    config.removeLoader("js").loader("js", {
-      test: /\.jsx?$/,
-      exclude: modulePath => {
-        return (
-          /node_modules/.test(modulePath) &&
-          !/node_modules\/(swiper|dom7)/.test(modulePath)
-        );
-      },
-      loader: "babel",
-      query: babelConfig,
-    });
-  });
-};
+// exports.modifyWebpackConfig = ({ config, stage }) => {
+//   const program = {
+//     directory: __dirname,
+//     browserslist: ["> 1%", "last 2 versions", "IE > 9"],
+//   };
+//
+//   return generateBabelConfig(program, stage).then(babelConfig => {
+//     config.removeLoader("js").loader("js", {
+//       test: /\.jsx?$/,
+//       exclude: modulePath => {
+//         return (
+//           /node_modules/.test(modulePath) &&
+//           !/node_modules\/(swiper|dom7)/.test(modulePath)
+//         );
+//       },
+//       loader: "babel",
+//       query: babelConfig,
+//     });
+//   });
+// };
 
 // exports.modifyWebpackConfig = ({ config, stage }) => {
 //   if (stage === "build-html") {
