88eb601b5160a62140623d4d2fc31339b8c48cea
Dan Kass <romeboards@gmail.com>
2018-06-18 16:08:14 -0400

    better env integration

--- src/components/ContentfulClient.js
+++ src/components/ContentfulClient.js
@@ -1,8 +1,8 @@
 import {createClient} from 'contentful';
 
 const client = createClient({
-  space: process.env.CONTENTFUL_SPACE_ID,
-  accessToken: process.env.CONTENTFUL_TOKEN
+  space: process.env.GATSBY_CONTENTFUL_SPACE_ID || process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.GATSBY_CONTENTFUL_TOKEN || process.env.CONTENTFUL_TOKEN
 });
 
 function fetchCommunityGroups(zipcode, locale) {
