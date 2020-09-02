50997c52bacbe4455957fdb0c24e2a71daf7f218
Sam Rabiyah <sraby@users.noreply.github.com>
2020-09-02 15:20:01 -0400

    Add Typescript and a barebones Jest test suite (#101)
    This PR adds a barebones Jest configuration to allow for basic test suites. It's modeled after JustFixNYC/justfix-website@9171e57.
    
    This PR also adds typescript and ts configurations as a regular dependency. We needed this now so we could make sure our Jest configuration is compatible with .tsx components, which we will inevitably be working with after converting some components to typescript.
    
    Fixes #85 and fixes #93.

---
+++ src/example.test.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import ButtonStep from "./components/ButtonStep";
+
+describe("Test suite", () => {
+  it("should run properly", () => {
+    expect("Boop").toBe("Boop");
+  });
+  it("works with tsx components too", () => {
+    const { container } = render(
+      <ButtonStep stepFn={() => {}}>Boop</ButtonStep>,
+    );
+    const span = container.querySelector("button");
+    expect(span?.textContent).toBe("Boop");
+  });
+});
