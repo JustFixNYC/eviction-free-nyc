90bd79d5f6965d065fd2820acee799a362cb3d8b
Atul Varma <varmaa@gmail.com>
2020-09-04 10:44:41 -0400

    Add source map support.

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,17 @@
 import babel from "@rollup/plugin-babel";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 
 const config = {
   input: "src/serverless-functions/hello-world.ts",
   output: {
     dir: "functions-build",
     format: "cjs",
+    sourcemap: "inline",
   },
   plugins: [
+    nodeResolve(),
+    commonjs(),
     babel({
       babelHelpers: "bundled",
       extensions: [".ts"],
--- src/serverless-functions/hello-world.ts
+++ src/serverless-functions/hello-world.ts
@@ -1,5 +1,13 @@
+import "source-map-support/register";
+
 import { upperCase } from "lodash";
 import { APIGatewayProxyHandler } from "aws-lambda";
+
+async function wait(seconds: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, seconds * 1000);
+  });
+}
 
 export const handler: APIGatewayProxyHandler = async (event) => {
   const kaboom =
@@ -9,8 +17,10 @@ export const handler: APIGatewayProxyHandler = async (event) => {
     throw new Error("Kaboom!");
   }
 
+  await wait(3);
+
   return {
     statusCode: 200,
-    body: upperCase("umm hi again"),
+    body: upperCase("umm hi again yup"),
   };
 };
