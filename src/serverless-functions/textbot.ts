64faecbb07f6d1e644fdf64d581e051b23d73060
Atul Varma <varmaa@gmail.com>
2020-09-14 07:53:51 -0400

    Simplify typings.

--- src/serverless-functions/textbot.ts
+++ src/serverless-functions/textbot.ts
@@ -1,4 +1,3 @@
-import { APIGatewayProxyResult } from "aws-lambda";
 import { handleConversation } from "../textbot/conversation-handlers";
 import { serverlessRollbarHandler } from "../utils/serverless-util";
 
@@ -6,10 +5,8 @@ export const handler = serverlessRollbarHandler(async (event) => {
   const query = event.queryStringParameters;
   const input = query ? query.input : undefined;
   const state = query ? query.state : undefined;
-  const response: APIGatewayProxyResult = {
+  return {
     statusCode: 200,
     body: JSON.stringify(await handleConversation(input || "", state)),
   };
-
-  return response;
 });
