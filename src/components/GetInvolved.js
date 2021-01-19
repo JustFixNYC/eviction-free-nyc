68bbce9d1292cec9ea4956287ee0f8e338ffd463
Sam Rabiyah <sraby@users.noreply.github.com>
2021-01-19 15:39:12 -0500

    Add content for interim EFNYC landing page (#136)
    This page adds new content to our landing page that acts as a placeholder before we launch our new EFNYC 2.0 tool with RTC/HJ4A

--- src/components/GetInvolved.js
+++ src/components/GetInvolved.js
@@ -63,7 +63,7 @@ const GetInvolved = () => (
             <script type="text/javascript" defer src="//www.123formbuilder.com/embed/3538714.js" data-role="form" data-default-width="650px"></script> */}
 
             <a
-              href="https://www.righttocounselnyc.org/join#middle"
+              href="https://www.righttocounselnyc.org/sign_up"
               target="_blank"
               className="btn btn-primary btn-block"
             >
@@ -82,7 +82,7 @@ const GetInvolved = () => (
               <Trans id="eventsTitle" />
             </label>
             <a
-              href="https://www.righttocounselnyc.org/upcoming_events#middle"
+              href="https://www.righttocounselnyc.org/blog"
               target="_blank"
               className="btn btn-primary btn-block"
             >
