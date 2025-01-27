b371a2d3c11c1c848d199dd367256222989a823e
Maxwell Austensen <m.austensen@gmail.com>
2025-01-27 09:17:35 -0500

    prettier

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,28 +15,22 @@ const Header = ({ intl }) => (
   <header className="Header navbar">
     <section className="navbar-section navbar-brand">
       <h1>
-        <Link to={`/${intl.locale}`}>
-          Eviction Free NYC
-        </Link>
+        <Link to={`/${intl.locale}`}>Eviction Free NYC</Link>
       </h1>
     </section>
     <section className="navbar-section navbar-collab">
       By{" "}
-        <a
-          href="https://www.righttocounselnyc.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          the Right to Counsel Coalition
-        </a>{" "}
-        &{" "}
-        <a
-          href="https://justfix.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          JustFix
-        </a>
+      <a
+        href="https://www.righttocounselnyc.org/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        the Right to Counsel Coalition
+      </a>{" "}
+      &{" "}
+      <a href="https://justfix.org/" target="_blank" rel="noopener noreferrer">
+        JustFix
+      </a>
     </section>
     <section className="navbar-section navbar-btns-social">
       <div className="btn-group btns-social">
